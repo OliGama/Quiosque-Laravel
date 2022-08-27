75c473e007f3a545341078623dc72fabcdea82c9
Matheus Wesley Lippi <102246955+matheuswlippi@users.noreply.github.com>
2022-08-26 21:35:28 -0300

    Atualizando

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,12 +119,12 @@
                             @csrf
                             <div class="row g-3">
                                 <div class="col-md-6">
-                                    <label for="inputEmail4" class="form-label">Email</label>
-                                    <input type="email" class="form-control" id="inputEmail4">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="email" name="email" class="form-control" id="email">
                                 </div>
                                 <div class="col-md-6">
-                                    <label for="inputPassword4" class="form-label">Senha</label>
-                                    <input type="password" class="form-control" id="inputPassword4">
+                                    <label for="password" class="form-label">Senha</label>
+                                    <input type="password" name="password" class="form-control" id="password">
                                 </div>
                                 <div class="col-12">
                                     <button type="submit" class="btn btn-primary">Login</button>
