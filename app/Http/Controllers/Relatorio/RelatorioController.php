b9e3cf1a420d1df87e16951ee8a064891ceb2126
mberganz <108034006+mberganz@users.noreply.github.com>
2022-11-17 22:28:41 -0300

    por favor funciona

--- app/Http/Controllers/Relatorio/RelatorioController.php
+++ app/Http/Controllers/Relatorio/RelatorioController.php
@@ -19,14 +19,24 @@ class RelatorioController extends Controller
 
     public function create(RelatorioRequest $request)
     {
-        $pedidos = DB::table('pedido_produto')->whereBetween('created_at', [date($request->dataInicio), date($request->dataFinal)])->get();
-        // dd($produtos);
-        $allProdutos = Produto::all();
-        $produto_nome = $allProdutos->pluck('nome_produto')->unique();
-        dd($produto_nome);
+        $pedidos = DB::table('pedido_produto')->whereBetween('created_at', [date($request->dataInicio), date($request->dataFinal)])->get()->groupBy('produto_id');
+        $allProdutos = Produto::all(['id', 'nome_produto', 'preco']);
+        $relatorio = [];
+
+        foreach ($pedidos as $key => $pedido) {
+            $tmp = $allProdutos->find($key);
+            $tmp_qnt = $pedido->sum('quantidade');
+            array_push($relatorio, [
+                'nome' => $tmp->nome_produto,
+                'quantidade' => $tmp_qnt,
+                'total' => $tmp_qnt * $tmp->preco
+            ]);
+        }
+
+
+        dd($relatorio);
+        // dd($produto_nome);
         return view('relatorio.show', [
-            'pedidos' => $pedidos,
-            'nome_produto' => $produto_nome
-        ]);
+            'pedidos' => $pedidos        ]);
     }
 }
--- resources/views/relatorio/show.blade.php
+++ resources/views/relatorio/show.blade.php
@@ -7,17 +7,18 @@
                 <th scope="col">Data</th>
                 <th scope="col">ID do Pedido</th>
                 <th scope="col">ID do Produto</th>
-                {{-- <th scope="col">Produto</th> --}}
+                <th scope="col">Produto</th>
                 <th scope="col">Quantidade</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($pedidos as $pedido)
                 <tr>
+                    {{-- @dd($pedido); --}}
                     <th>{{ $pedido->created_at }}</th>
                     <td>{{ $pedido->pedido_id }}</td>
                     <td>{{ $pedido->produto_id }}</td>
-                    {{-- <td>{{ $pedido->produto_nome }}</td> --}}
+                    <td>{{ $pedido->pivot->nome_produto }}</td>
                     <td>{{ $pedido->quantidade }}</td>
                 </tr>
             @endforeach
